d9b798300e344a6a2bfa395d16f9a97f998652cf
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-11-20 16:01:30 +0900

    Title: 이상한 진수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231120/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,14 @@
+a = input()
+a = [*a]
+max_a = 0
+
+for i in range(1, len(a)):
+    if a[i] == '0':
+        a[i] = '1'
+        n = ''.join(a)
+        x = int(n, 2)
+        a[i] = '0'
+    if x > max_a:
+        max_a = x
+
+print(max_a)
