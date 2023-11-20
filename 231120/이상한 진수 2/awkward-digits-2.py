04f8bf2dbd75b9e3b19d49e4c9a306372102a7be
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-11-20 16:13:10 +0900

    Title: 이상한 진수 2, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 231120/이상한 진수 2/awkward-digits-2.py
+++ 231120/이상한 진수 2/awkward-digits-2.py
@@ -1,14 +1,18 @@
 a = list(input())
 max_a = 0
 
-for i in range(1, len(a)):
-    if a[i] != '0':
-        continue
-    a[i] = '1'
-    n = ''.join(a)
-    x = int(n, 2)
-    a[i] = '0'
-    if x > max_a:
-        max_a = x
+if '0' not in a:
+    a[-1] = '0'
+    print(int(''.join(a), 2))
+else:
+    for i in range(1, len(a)):
+        if a[i] != '0':
+            continue
+        a[i] = '1'
+        n = ''.join(a)
+        x = int(n, 2)
+        a[i] = '0'
+        if x > max_a:
+            max_a = x
 
-print(max_a)
+    print(max_a)
