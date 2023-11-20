2e6a073a2eab9123209d9487ba1bc1b8a24ccfa1
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-11-20 16:10:56 +0900

    Title: 이상한 진수 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231120/이상한 진수 2/awkward-digits-2.py
+++ 231120/이상한 진수 2/awkward-digits-2.py
@@ -1,13 +1,13 @@
-a = input()
-a = [*a]
+a = list(input())
 max_a = 0
 
 for i in range(1, len(a)):
-    if a[i] == '0':
-        a[i] = '1'
-        n = ''.join(a)
-        x = int(n, 2)
-        a[i] = '0'
+    if a[i] != '0':
+        continue
+    a[i] = '1'
+    n = ''.join(a)
+    x = int(n, 2)
+    a[i] = '0'
     if x > max_a:
         max_a = x
 
