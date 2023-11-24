38ea253ee655446f2f3c10f5931214b26c082604
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-11-25 00:19:59 +0900

    Title: 마라톤 중간에 택시타기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231125/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,21 @@
+n = int(input())
+path = []
+for _ in range(n):
+    x,y = map(int, input().split())
+    path.append((x,y))
+
+paths = path.copy()
+min_dist = 9999
+
+for i in range(1, len(path)-1):
+    dist = 0
+    paths.pop(i)
+    for j in range(len(paths)-1):
+        x = abs(paths[j][0] - paths[j+1][0])
+        y = abs(paths[j][1] - paths[j+1][1])
+        dist += x+y
+    if dist < min_dist:
+        min_dist = dist
+    paths = path
+
+print(min_dist)
