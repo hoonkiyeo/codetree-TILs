b085afe4ad03fd64cb607a0c3223a30e24dc8284
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-11-27 15:34:40 +0900

    Title: Carry 피하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231127/Carry 피하기 2/escaping-carry-2.py
+++ 231127/Carry 피하기 2/escaping-carry-2.py
@@ -16,4 +16,7 @@ for i in range(len(nums)-2):
                                 num = x+y+z
                                 if num > max_n:
                                     max_n = num
-print(max_n)
+if max_n = 0:
+    print(-1)
+else:
+    print(max_n)
