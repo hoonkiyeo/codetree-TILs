1fb1989822fa84ee0a3db6e888fe311266dc6b56
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-01 00:37:01 +0900

    Title: 오목, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 231201/오목/O-mok.py
+++ 231201/오목/O-mok.py
@@ -2,66 +2,32 @@ arr = []
 for _ in range(19):
     arr.append(list(map(int, input().split())))
 
-black = 0
-white = 0
-is_black = False
-is_white = False
-idx_b = [0,0]
-idx_w = [0,0]
+def is_winner(arr, player):
+    for i in range(19):
+        for j in range(19):
+            if arr[i][j] == player:
+                # Check horizontally
+                if j <= 14 and all(arr[i][j + k] == player for k in range(1, 5)):
+                    return True, [i + 1, j + 3]
+                # Check vertically
+                if i <= 14 and all(arr[i + k][j] == player for k in range(1, 5)):
+                    return True, [i + 3, j + 1]
+                # Check diagonal down-right
+                if i <= 14 and j <= 14 and all(arr[i + k][j + k] == player for k in range(1, 5)):
+                    return True, [i + 3, j + 3]
+                # Check diagonal up-right
+                if i >= 4 and j <= 14 and all(arr[i - k][j + k] == player for k in range(1, 5)):
+                    return True, [i - 1, j + 3]
+    return False, []
 
-def check_black(arr,x,y):
-    if arr[x][y] == 1:
-        return True
-    return False
-def check_white(arr,x,y):
-    if arr[x][y] == 2:
-        return True
-    return False
-def in_range(x,y):
-    return x >= 0 and y >= 0 and x <= 18 and y <= 18
-
-for i in range(19):
-    for j in range(19):
-        if arr[i][j] == 1: # black
-            if check_black(arr, i, j+1) and check_black(arr, i, j+2) and check_black(arr, i, j+3) and check_black(arr, i, j+4):
-                idx_b = [i,j+2]
-                is_black = True
-                break
-            elif check_black(arr, i+1, j) and check_black(arr, i+2, j) and check_black(arr, i+3, j) and check_black(arr, i+4, j):
-                idx_b = [i+2,j]
-                is_black = True
-                break
-            elif check_black(arr, i+1, j+1) and check_black(arr, i+2, j+2) and check_black(arr, i+3, j+3) and check_black(arr, i+4, j+4):
-                idx_b = [i+2, j+2]
-                is_black = True
-                break
-            elif check_black(arr, i+1, j-1) and check_black(arr, i+2, j-2) and check_black(arr, i+3, j-3) and check_black(arr, i+4, j-4):
-                idx_b = [i+2, j-2]
-                is_black = True
-                break
-        elif arr[i][j] == 2: # white
-            if check_white(arr, i, j+1) and check_white(arr, i, j+2) and check_white(arr, i, j+3) and check_white(arr, i, j+4):
-                idx_w = [i,j+2]
-                is_white = True
-                break
-            elif check_white(arr, i+1, j) and check_white(arr, i+2, j) and check_white(arr, i+3, j) and check_white(arr, i+4, j):
-                idx_w = [i+2,j]
-                is_white = True
-                break
-            elif check_white(arr, i+1, j+1) and check_white(arr, i+2, j+2) and check_white(arr, i+3, j+3) and check_white(arr, i+4, j+4):
-                idx_w = [i+2, j+2]
-                is_white = True
-                break
-            elif check_white(arr, i+1, j-1) and check_white(arr, i+2, j-2) and check_white(arr, i+3, j-3) and check_white(arr, i+4, j-4):
-                idx_w = [i+2, j-2]
-                is_white = True
-                break
+is_black, black_idx = is_winner(arr, 1)
+is_white, white_idx = is_winner(arr, 2)
 
 if is_black:
     print(1)
-    print(idx_b[0]+1, idx_b[1]+1)
+    print(*black_idx)
 elif is_white:
     print(2)
-    print(idx_w[0]+1, idx_w[1]+1)
+    print(*white_idx)
 else:
     print(0)
