78f0eaaf3b90186687c2051a86c07f89ecc44765
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-01 12:33:33 +0900

    Title: 최고의 13위치 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231201/최고의 13위치 2/best-place-of-13-2.py
+++ 231201/최고의 13위치 2/best-place-of-13-2.py
@@ -14,11 +14,12 @@ for i in range(n):
         if cnt > max_cnt:
             max_cnt = cnt
             idx = (i,j)
+
 # second max_cnt
 for i in range(n):
     for j in range(n-2):
         cnt = 0
-        if (i == idx[0] and j == idx[1]) or j == idx[1]-1 or j == idx[1]-2 or j == idx[1]+1 or j == idx[1]+2:
+        if i == idx[0] and idx[1] - 2 <= j <= idx[1] + 2:
             continue 
         cnt += (arr[i][j] + arr[i][j+1] + arr[i][j+2])
         if cnt > max_cnt2:
