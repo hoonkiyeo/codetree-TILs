cabbb1a30ecbdf448d9bd2471243705bb2758ae3
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-01 12:21:22 +0900

    Title: 최고의 13위치 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231201/최고의 13위치 2/best-place-of-13-2.py
+++ 231201/최고의 13위치 2/best-place-of-13-2.py
@@ -8,19 +8,19 @@ max_cnt2 = 0
 idx = 0
 # max_cnt
 for i in range(n):
-    cnt = 0
     for j in range(n-2):
+        cnt = 0
         cnt += (arr[i][j] + arr[i][j+1] + arr[i][j+2])
-    if cnt > max_cnt:
-        max_cnt = cnt
-        idx = (i,j)
+        if cnt > max_cnt:
+            max_cnt = cnt
+            idx = (i,j)
 # second max_cnt
 for i in range(n):
-    cnt = 0
     for j in range(n-2):
+        cnt = 0
         if i == idx[0] and j == idx[1]:
             continue 
         cnt += (arr[i][j] + arr[i][j+1] + arr[i][j+2])
-    if cnt > max_cnt2:
-        max_cnt2 = cnt
+        if cnt > max_cnt2:
+            max_cnt2 = cnt
 print(max_cnt + max_cnt2)
