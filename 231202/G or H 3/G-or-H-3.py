06d639b405195af1c49b8b3da9aa2537da2a02c2
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-02 13:35:22 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231202/G or H 3/G-or-H-3.py
+++ 231202/G or H 3/G-or-H-3.py
@@ -1,20 +1,14 @@
 n,k = map(int, input().split())
-arr = []
-for i in range(n):
-    arr.append(input().split())
-    arr[i][0] = int(arr[i][0])
-
-placed = [0] * 10001 
-for ele in arr:
-    if ele[1] == 'G':
-        placed[ele[0]] = 1
-    elif ele[1] == 'H':
-        placed[ele[0]] = 2
+positions = [0] * 10001
+for _ in range(n):
+    pos, letter = input().split()
+    pos = int(pos)
+    positions[pos] = 1 if letter == 'G' else 2
 
 max_score = 0
 for i in range(1, k+1):
     score = 0
     for j in range(i, i+k+1):
-        score += placed[j]
+        score += positions[j]
     max_score = max(max_score, score)
 print(max_score)
