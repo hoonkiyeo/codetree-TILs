6e297f879264662a244203c3df99913cacffdd45
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-02 14:59:41 +0900

    Title: G or H 3, Time: 170ms, Memory: 28MB, Status: Passed - CodeTree

--- 231202/G or H 3/G-or-H-3.py
+++ 231202/G or H 3/G-or-H-3.py
@@ -1,14 +1,18 @@
 n,k = map(int, input().split())
 positions = [0] * 10001
+max_num = 0
 for _ in range(n):
     pos, letter = input().split()
     pos = int(pos)
     positions[pos] = 1 if letter == 'G' else 2
+    max_num = max(pos, max_num)
 
-max_score = 0
-for i in range(1, k+1):
-    score = 0
-    score += sum(positions[i:i+k+1])
-    max_score = max(max_score, score)
-
-print(max_score)
+if k > max_num:
+    print(sum(positions))
+else:
+    max_score = 0
+    for i in range(1, max_num-k+1):
+        score = 0
+        score += sum(positions[i:i+k+1])
+        max_score = max(max_score, score)
+    print(max_score)
