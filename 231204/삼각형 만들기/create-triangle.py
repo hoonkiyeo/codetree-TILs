7b908a4a24e993a9b09474775641d853b180ffe9
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-04 14:26:42 +0900

    Title: 삼각형 만들기, Time: 157ms, Memory: 25MB, Status: Passed - CodeTree

--- 231204/삼각형 만들기/create-triangle.py
+++ 231204/삼각형 만들기/create-triangle.py
@@ -11,9 +11,7 @@ for i in range(n):
             x1,y1 = coords[i]
             x2,y2 = coords[j] 
             x3,y3 = coords[k]
-            if (x1 == x2 and y2 == y3) or (x1 == x3 and y3 == y2) or \
-               (x2 == x3 and y3 == y1) or (x2 == x1 and y1 == y3) or \
-               (x3 == x1 and y1 == y2) or (x3 == x2 and y2 == y1):
-                area = 1/2*abs((x1*x2 + x2*y3 + x3*y1) - (x2*y2 + x3*y2 + x1*y3))
+            if (x1 == x2 or x1 == x3 or x2 == x3) and (y1 == y2 or y1 == y3 or y2 == y3):
+                area = 1/2*abs((x1*y2 + x2*y3 + x3*y1) - (x2*y1 + x3*y2 + x1*y3))
                 max_area = max(max_area, area)
 print(int(max_area * 2))
