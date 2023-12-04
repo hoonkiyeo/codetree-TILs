77e7bc8a8af20467e1ea9e023495e9682973e2fd
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-04 11:38:01 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231204/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
+++ 231204/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -5,12 +5,20 @@ for _ in range(n):
     coords.append((x,y))
 
 min_dist = 9999999
+idx = (0,0)
 for i in range(n):
     for j in range(i+1, n):
         if i == j:
             continue
         x1,y1 = coords[i][0], coords[i][1]
         x2,y2 = coords[j][0], coords[j][0]
-        sdist = abs(x1-x2) * abs(x1-x2) + abs(y1-y2) * abs(y1-y2)
-        min_dist = min(min_dist, sdist)
-print(min_dist)
+        dist = abs(x1-x2) + abs(y1-y2)
+        if dist < min_dist:
+            min_dist = dist
+            idx = (i,j)
+
+i,j = idx[0], idx[1]
+x1,y1 = coords[i][0], coords[i][1]
+x2,y2 = coords[j][0], coords[j][1]
+s_dist = abs(x1-x2) * abs(x1-x2) + abs(y1-y2) * abs(y1-y2)
+print(s_dist)
