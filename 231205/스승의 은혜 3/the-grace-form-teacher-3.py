8ae8358022a94a98eb71b6eee1001460a1b5106c
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-05 16:22:22 +0900

    Title: 스승의 은혜 3, Time: 138ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231205/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,27 @@
+n,b = map(int, input().split())
+presents = []
+for _ in range(n):
+    p,s = map(int, input().split())
+    presents.append((p,s))
+presents = sorted(presents, key=lambda x:x[0]+x[1])
+
+max_cnt = 0
+for i in range(n):
+    cnt = 0
+    total = 0
+
+    p,s = presents[i]
+    p = p // 2
+    total += (p+s)
+    cnt += 1
+    for j in range(n):
+        if i == j:
+            continue
+        if total >= b:
+            break
+        else:
+            p,s = presents[j]
+            total += (p+s)
+            cnt += 1
+    max_cnt = max(max_cnt, cnt)
+print(cnt)
