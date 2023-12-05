fda607d2cb022953401bd8f4fabd7fc734ef89f2
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-05 11:22:54 +0900

    Title: 상해버린 치즈, Time: 144ms, Memory: 25MB, Status: Passed - CodeTree

--- 231205/상해버린 치즈/rotten-cheese.py
+++ 231205/상해버린 치즈/rotten-cheese.py
@@ -1,50 +1,67 @@
-# n: 사람의 수
-# m: 치즈의 수
-# d: 먹은 기록의 수
-# s: 아픈 기록의 수
-N,M,D,S = map(int,input().split())
+# 클래스 선언
+class Info1:
+    def __init__(self, p, m, t):
+        self.p, self.m, self.t = p, m, t
 
-arr = []
-for _ in range(D):
-    p,m,t = map(int, input().split())
-    # p: 몇번 째 사람
-    # m: 몇 번째 치즈
-    # t: 먹은 시점 (초)
-    arr.append((p,m,t))
+class Info2:
+    def __init__(self, p, t):
+        self.p, self.t = p, t
 
-sick = []
-sick_p = []
-sick_cnt = 0
-for _ in range(S):
-    p,t = map(int,input().split())
-    # p: 아팠던 사람
-    # t: 아팠던 시점
-    sick.append((p,t))
-    sick_p.append(p)
-    sick_cnt += 1
+# 변수 선언 및 입력
+n, m, d, s = tuple(map(int, input().split()))
 
-bad_cheese = [0] * (M+1)
-final_cheese = []
-for i in range(len(sick)):
-    person = sick[i][0]
-    time = sick[i][1]
-    for j in range(len(arr)):
-        person2 = arr[j][0]
-        cheese = arr[j][1]
-        time2 = arr[j][2]
-        if person == person2:
-            if time2 < time:
-                bad_cheese[cheese] += 1
-for i in range(len(bad_cheese)):
-    if bad_cheese[i] == sick_cnt:
-        final_cheese.append(i)
+info1 = []
+for _ in range(d):
+    p, x, t = tuple(map(int, input().split()))
+    info1.append(Info1(p, x, t))
 
-sick_cnt2 = 0
-for i in range(len(arr)):
-    p,m,t = arr[i][0], arr[i][1], arr[i][2]
-    if p in sick_p:
-        continue
-    if m in final_cheese:
-        sick_cnt2 += 1
+info2 = []
+for _ in range(s):
+    p, t = tuple(map(int, input().split()))
+    info2.append(Info2(p, t))
 
-print(sick_cnt + sick_cnt2)
+ans = 0
+    
+# 하나의 치즈가 상했을 때 필요한 약의 수의 최댓값을 구합니다.
+for i in range(1, m + 1):
+    # i번째 치즈가 상했을 때 필요한 약의 수를 구합니다.
+
+    # 우선 i번째 치즈가 상했다고 가정할 때 모순이 발생하지 않는지 확인합니다.
+    # time배열을 만들어 각 사람이 언제 치즈를 먹었는지 저장합니다.
+    time = [0] * (n + 1)
+    for info in info1:
+        # i번째 치즈에 대한 정보가 아닌 경우 넘어갑니다.
+        if info.m != i:
+            continue
+
+        # person이 i번째 치즈를 처음 먹었거나
+        # 이전보다 더 빨리 먹게 된 경우 time배열을 갱신합니다.
+        person = info.p
+        if time[person] == 0:
+            time[person] = info.t
+        elif time[person] > info.t:
+            time[person] = info.t
+
+    # possible : i번째 치즈가 상했을 수 있으면 true, 아니면 false
+    possible = True
+
+    for info in info2:
+        # person이 i번째 치즈를 먹지 않았거나
+        # i번째 치즈를 먹은 시간보다 먼저 아픈 경우 모순이 생깁니다.
+        person = info.p
+        if time[person] == 0:
+            possible = False
+        if time[person] >= info.t:
+            possible = False
+
+    # 만약 i번째 치즈가 상했을 가능성이 있다면, 몇 개의 약이 필요한지 확인합니다.
+    pill = 0
+    if possible:
+        # 한번이라도 i번째 치즈를 먹은 적이 있다면, 약이 필요합니다.
+        for j in range(1, n + 1):
+            if time[j] != 0:
+                pill += 1
+
+        ans = max(ans, pill);
+
+print(ans)
