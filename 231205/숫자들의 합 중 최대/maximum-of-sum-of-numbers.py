9a4563c766483384ac12a5565bde6b8a14fca961
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-05 18:21:50 +0900

    Title: 숫자들의 합 중 최대, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

--- 231205/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
+++ 231205/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
@@ -1,9 +1,12 @@
 x,y = map(int, input().split())
 
-
-
 max_sum = 0
-for i in range(x,y+1):
-    total_sum = (i % 10) + (i // 10) + (i // 100) + (i // 1000) + (i // 10000)
+for n in range(x,y+1):
+    units = int(str(n // 1)[-1])
+    tens = int(str(n // 10)[-1])
+    hundreds = int(str(n // 100)[-1])
+    thousands = int(str(n // 1000)[-1])
+    ten_thousands = int(str(n // 10000)[-1])
+    total_sum = units + tens + hundreds + thousands + ten_thousands
     max_sum = max(max_sum, total_sum)
 print(max_sum)
