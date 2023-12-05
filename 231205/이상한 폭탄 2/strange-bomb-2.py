06fab5ed13d9cdd10d8f3b8aad559f8fcc2e346e
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-05 14:44:12 +0900

    Title: 이상한 폭탄 2, Time: 140ms, Memory: 28MB, Status: Passed - CodeTree

--- 231205/이상한 폭탄 2/strange-bomb-2.py
+++ 231205/이상한 폭탄 2/strange-bomb-2.py
@@ -4,7 +4,7 @@ bombs = [int(input()) for _ in range(n)]
 max_bomb = -1
 for i in range(n):
     for j in range(i+1, n):
-        if j - i > 3:
+        if j - i > k:
             continue
         if bombs[i] == bombs[j]:
             max_bomb = max(max_bomb, bombs[i])
