28f12bf6a7e0610c2f86f1ad405a829616293695
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-06 12:59:48 +0900

    Title: 정보에 따른 숫자 2, Time: 152ms, Memory: 25MB, Status: Passed - CodeTree

--- 231206/정보에 따른 숫자 2/number-based-on-information-2.py
+++ 231206/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -1,19 +1,18 @@
 t,a,b = map(int, input().split())
-arr = [0] * (b+1)
-
+pos = []
 for _ in range(t):
     c,x = input().split()
     x = int(x)
-    arr[x] = c
+    pos.append((c,x))
 
-s_indices = [i for i in range(a, b+1) if arr[i] == 'S']
-n_indices = [i for i in range(a, b+1) if arr[i] == 'N']
+s_indices = [x for a,x in pos if a == 'S']
+n_indices = [x for a,x in pos if a == 'N']
 special_cnt = 0
 
 for i in range(a,b+1):
     closest_s = min([abs(i-s) for s in s_indices])
     closest_n = min([abs(i-n) for n in n_indices])
-
     if closest_s <= closest_n:
         special_cnt += 1
+
 print(special_cnt)
