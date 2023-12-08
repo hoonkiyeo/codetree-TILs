77040484d28a0dad8bb3749febbbbb1d9a5a0aaa
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-08 14:01:20 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 162ms, Memory: 25MB, Status: Passed - CodeTree

--- 231208/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 231208/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -1,15 +1,23 @@
 n = int(input())
 string = input()
 
+
+def find_string(s, target):
+    cnt = 0
+    for i in range(len(s)):
+        if s[i:i+len(target)] == target:
+            cnt += 1
+    return cnt
+
 min_length = 0
 for i in range(1,n):
     is_bool = True
     for j in range(n-i+1):
-        consecutive_string = string[j:j+i]
-        cnt = string.count(consecutive_string)
+        target_string = string[j:j+i]
+        cnt = find_string(string, target_string)
         if cnt >= 2:
             is_bool = False
     if is_bool:
-        min_length = len(consecutive_string)
+        min_length = len(target_string)
         break
 print(min_length)
