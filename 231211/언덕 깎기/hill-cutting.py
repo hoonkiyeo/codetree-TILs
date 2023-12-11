3fb19d793817df180e7f28b3ee55539dd553e5c6
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-11 11:55:26 +0900

    Title: 언덕 깎기, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 231211/언덕 깎기/hill-cutting.py
+++ 231211/언덕 깎기/hill-cutting.py
@@ -2,19 +2,14 @@ n = int(input())
 arr = [int(input()) for _ in range(n)]
 
 min_cost = float('inf')
-for i in range(n):
-    for j in range(i+1,n):
-        max_h = max(arr[i], arr[j])
-        min_h = min(arr[i], arr[j])
-        cost = 0
-        if max_h - min_h > 17:
-            continue
-        for k in range(n):
-            if k == i or k == j:
-                continue
-            if arr[k] < min_h:
-                cost += (min_h - arr[k]) ** 2
-            elif arr[k] > max_h:
-                cost += (arr[k] - max_h) ** 2
-        min_cost = min(min_cost, cost)
+for min_h in range(84):  # 0부터 83까지
+    max_h = min_h + 17
+    cost = 0
+    for k in range(n):
+        if arr[k] < min_h:
+            cost += (min_h - arr[k]) ** 2
+        elif arr[k] > max_h:
+            cost += (arr[k] - max_h) ** 2
+    min_cost = min(min_cost, cost)
+
 print(min_cost)
