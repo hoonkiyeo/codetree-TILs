7c95394123a0bf5ec420438f14fa6a957e7a2f84
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-11 15:12:09 +0900

    Title: 구간 잘 나누기, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 231211/구간 잘 나누기/divide-sections-well.py
+++ 231211/구간 잘 나누기/divide-sections-well.py
@@ -1,18 +1,17 @@
 import sys
-max_ = -sys.maxsize
-min_ = sys.maxsize
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-if n == m:
-    print(max(arr))
-else:
-    divider = n // m
-    for i in range(0, n, divider):
-        cnt = 0
-        for j in range(i, i + divider):
-            cnt += arr[j]
-        max_ = max(max_, cnt)
-        min_ = min(min_, max_)
-
-    print(min_)
+for i in range(max(arr), sum(arr)+1):
+    tmp = []
+    val_sum = arr[0]
+    for j in range(1, n):
+        if val_sum + arr[j] > i:
+            tmp.append(val_sum)
+            val_sum = arr[j]
+        else:
+            val_sum += arr[j]
+    tmp.append(val_sum)
+    if len(tmp) <= m:
+        print(i)
+        break
