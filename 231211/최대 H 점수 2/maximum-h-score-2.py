f54a197c740a03bcca22cd08473aa72da1c84052
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-11 18:27:34 +0900

    Title: 최대 H 점수 2, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 231211/최대 H 점수 2/maximum-h-score-2.py
+++ 231211/최대 H 점수 2/maximum-h-score-2.py
@@ -1,35 +1,20 @@
-from collections import Counter
+n,l = map(int, input().split())
+a = list(map(int, input().split()))
 
-def calculate_h_score(sequence):
-    sequence.sort(reverse=True)
-    h_score = 0
-    for i, num in enumerate(sequence):
-        if i + 1 >= num:
-            h_score = max(h_score, num)
-    return h_score
+ans = 0
+for i in range(1, n+1):
 
-def max_h_score(N, L, sequence):
-    count = Counter(sequence)
-    most_common = count.most_common()
+    cnt = 0
+    cntl = 0
 
-    for num, freq in most_common:
-        if L <= 0:
-            break
-        increase = min(L, freq)
-        L -= increase
-        for i in range(len(sequence)):
-            if sequence[i] == num:
-                sequence[i] += 1
-                increase -= 1
-                if increase == 0:
-                    break
+    for j in range(n):
+        if a[j] >= i:
+            cnt += 1
+        elif a[j] == i-1:
+            if cntl < l:
+                cntl += 1
+                cnt += 1
+    if cnt >= i:
+        ans = i
 
-    return calculate_h_score(sequence)
-
-def main():
-    N, L = map(int, input().split())
-    sequence = list(map(int, input().split()))
-    print(max_h_score(N, L, sequence))
-
-if __name__ == "__main__":
-    main()
+print(ans)
