b71ab75b1f731fa69539692ea1c83609e63c0c78
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-12 12:37:06 +0900

    Title: 훌륭한 점프, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 231212/훌륭한 점프/great-jump.py
+++ 231212/훌륭한 점프/great-jump.py
@@ -1,25 +1,15 @@
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
-# def is_possible(max_val):
-#     last_idx = 0
-#     for i in range(1, n):
-#         if arr[i] <= max_val:
-#             if i - last_idx > k:
-#                 return False
-#             last_idx = i
-#     return True
-
 def is_possible(max_val):
     indices = [0]
     for i in range(1,n):
         if arr[i] <= max_val:
             indices.append(i)
     for i in range(len(indices)-1):
-        if indices[i+1] - indices[i] > 2:
+        if indices[i+1] - indices[i] > k:
             return False
     return True
-
 
 for i in range(max(arr[0], arr[n-1]), 101):
     if is_possible(i):
