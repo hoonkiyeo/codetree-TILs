47551b37d38f0b7f0d1d2f385f17662e2ead47f0
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-13 15:39:28 +0900

    Title: 비둘기와 전기줄, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 231213/비둘기와 전기줄/pigeons-and-electric-cords.py
+++ 231213/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -8,7 +8,19 @@ for _ in range(n):
 for x,y in pos:
     dic[x].append(y)
 
+# val_sum = 0
+# for bird in dic.keys():
+#     val_sum += min(dic[bird].count(0), dic[bird].count(1))
+# print(val_sum)
+
 val_sum = 0
-for bird in dic.keys():
-    val_sum += min(dic[bird].count(0), dic[bird].count(1))
+for key in dic.keys():
+    bird = dic[key]
+    cnt = 0
+    if len(bird) < 2:
+        continue
+    for i in range(1, len(bird)):
+        if bird[i] != bird[i-1]:
+            cnt += 1
+    val_sum += cnt
 print(val_sum)
