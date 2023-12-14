8f5eb892aa1eb95fed5978ac30cc7194e45df08e
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-14 14:23:50 +0900

    Title: 연속된 숫자 만들기 2, Time: 199ms, Memory: 24MB, Status: Passed - CodeTree

--- 231214/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 231214/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -1,6 +1,8 @@
 arr = list(map(int, input().split()))
 
-if arr[1] - arr[0] == 2 or arr[2] - arr[1] == 2:
+if arr[1] - arr[0] == 1 and arr[2] - arr[1] == 1:
+    print(0)
+elif arr[1] - arr[0] == 2 or arr[2] - arr[1] == 2:
     print(1)
 else:
     print(2)
