fb11367c18405167d3204ec78845dfab26fbe38d
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-14 14:21:23 +0900

    Title: 연속된 숫자 만들기 2, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231214/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 231214/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -1,17 +1,6 @@
 arr = list(map(int, input().split()))
-cnt = 0
-arr1 = arr.copy()
-arr2 = arr.copy()
-for _ in range(100):
-    cnt += 1
-    min_val = min(arr[0], arr[-1])
-    max_val = max(arr[0], arr[-1])
-    min_idx = arr.index(min_val)
-    max_idx = arr.index(max_val)
-    arr1[min_idx], arr1[1] = arr1[1], arr1[min_idx]
-    arr2[max_idx], arr2[1] = arr2[1], arr2[max_idx]
-    
-    if arr1 == sorted(arr1) or arr2 == sorted(arr2):
-        break
 
-print(cnt)
+if arr[1] - arr[0] <= 2 and arr[2] - arr[1] <= 2:
+    print(1)
+else:
+    print(2)
