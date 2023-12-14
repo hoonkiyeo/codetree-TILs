6a444e3f3c96477d6f61dab20016a449f9e757b6
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-14 12:37:37 +0900

    Title: 코딩톡, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231214/코딩톡/coding-talk.py
@@ -0,0 +1,21 @@
+import sys
+
+n, m, p = tuple(map(int, input().split()))
+message = [
+    list(input().split())
+    for _ in range(m)
+]
+
+# 모두가 읽은 메세지일 경우
+if int(message[p-1][1]) == 0:
+    sys.exit()
+
+for i in range(n):
+    person = chr(ord('A') + i)
+    read = False
+    for c,u in message:
+        u = int(u)
+        if u >= int(message[p-1][1]) and c == person:
+            read = True
+    if read == False:
+        print(person, end=' ')
