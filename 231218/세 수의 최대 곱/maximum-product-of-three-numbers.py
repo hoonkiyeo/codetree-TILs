ef9186dac61224b6e86bbb2ac65435c775765601
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2023-12-18 10:36:38 +0900

    Title: 세 수의 최대 곱, Time: 171ms, Memory: 31MB, Status: Passed - CodeTree

--- 231218/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 231218/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -1,11 +1,5 @@
 n = int(input())
 nums = list(map(int, input().split()))
+nums.sort()
 
-max_mul = 0
-
-for i in range(n):
-    for j in range(i+1, n):
-        for k in range(j+1, n):
-            val = nums[i] * nums[j] * nums[k]
-            max_mul = max(max_mul, val)
-print(max_mul)
+print(max(nums[0] * nums[1] * nums[-1], nums[-1] * nums[-2] * nums[-3]))
