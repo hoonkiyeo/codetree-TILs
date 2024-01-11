35d7918d6b334be5cd29336d094897d44462091a
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2024-01-11 10:57:13 +0900

    Title: 기수 정렬 구현, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240111/기수 정렬 구현/implement-radix-sort.py
+++ 240111/기수 정렬 구현/implement-radix-sort.py
@@ -1,25 +1,10 @@
-# function radix_sort(arr, k)
-#   for pos = k - 1 ... pos >= 0:
-#     set arr_new = [10][]
-#     for i = 0 ... i < arr.size
-#       set digit = posth digit of arr[i]
-#       arr_new[digit].append(arr[i])
-
-#     set store_arr = []
-#     for i = 0 ... i < 10
-#       for j = 0 ... j < arr_new[i].size
-#         store_arr.append(arr_new[i][j])
-  
-#     arr = store_arr
-
-#   return arr
-
 n = int(input())
 arr = list(map(int, input().split()))
 k = len(str(max(arr)))
 
 for pos in range(k)[::-1]:
-  arr_new = [[0] for _ in range(10)]
+  arr_new = [[] for _ in range(10)]
+
   for i in range(n):
     digit = int(str(arr[i])[pos])
     arr_new[digit].append(arr[i])
