07555da32ab18f122e9e7e633c36daaefcb23692
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2024-01-11 11:01:08 +0900

    Title: 기수 정렬 구현, Time: 196ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240111/기수 정렬 구현/implement-radix-sort.py
+++ 240111/기수 정렬 구현/implement-radix-sort.py
@@ -2,20 +2,14 @@ n = int(input())
 arr = list(map(int, input().split()))
 k = len(str(max(arr)))
 
-for pos in range(k)[::-1]:
-  arr_new = [[] for _ in range(10)]
+for i in range(k):    
+    digit = [[] for _ in range(10)]
+    for num in arr:
+        digit[(num//(10**i))%10].append(num)
+    numbers = []
 
-  for i in range(n):
-    digit = int(str(arr[i])[pos])
-    arr_new[digit].append(arr[i])
-  
-  store_arr = []
-  for i in range(10):
-    for j in range(len(arr_new[i])):
-      store_arr.append(arr_new[i][j])
-  
-  arr = store_arr
-
-for ele in arr:
-  if ele > 0:
-    print(ele, end=' ')
+    for d in range(10) :
+        for num in digit[d] :
+            numbers.append(num)
+for ele in numbers:
+  print(ele, end=' ')
