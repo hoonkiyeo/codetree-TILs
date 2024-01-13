679b8f2545e2130316afb78e2de544cf10c05b8b
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2024-01-13 11:19:07 +0900

    Title: 퀵 정렬 구현, Time: 312ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 240113/퀵 정렬 구현/implement-quick-sort.py
@@ -0,0 +1,26 @@
+n = int(input())
+arr = list(map(int, input().split()))
+low = 0
+high = n-1
+
+def partition(low, high):
+    pivot = arr[high]
+    i = low-1
+
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+
+    return i+1
+
+def quick_sort(low, high):
+    if low < high:
+        pos = partition(low, high)
+
+        quick_sort(low, pos-1)
+        quick_sort(pos+1, high)
+
+quick_sort(low, high)
+print(*arr)
