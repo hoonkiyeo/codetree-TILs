2fb0c85864b3350965c4dde722ae632375de3250
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2024-02-08 15:05:30 +0900

    Title: 행복한 수열의 개수, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240208/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240208/행복한 수열의 개수/number-of-happy-sequence.py
@@ -7,11 +7,11 @@ if m == 1:
 else:
     def is_happy_arr(seq, m):
         max_cnt = 0
-        consecutive_cnt = 0
+        consecutive_cnt = 1
 
         for i in range(len(seq)-1):
             if seq[i] != seq[i+1]:
-                consecutive_cnt = 0
+                consecutive_cnt = 1
             consecutive_cnt += 1
             max_cnt = max(max_cnt, consecutive_cnt)
         
